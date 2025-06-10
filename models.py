f11630cbdab70133acf2d43bb5def4999290ad97
eyfahrlehrer <info@machmichmobiler.com>
2025-06-11 01:14:10 +0200

    Update models.py

--- models.py
+++ models.py
@@ -17,3 +17,16 @@ class Schueler(db.Model):
     theorie_bestanden = db.Column(db.Boolean, default=False)
     fahrerlaubnisklasse = db.Column(db.String(10))
     erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Fahrstundenprotokoll(db.Model):
+    __tablename__ = 'fahrstundenprotokoll'
+
+    id = db.Column(db.Integer, primary_key=True)
+    schueler_id = db.Column(db.Integer, db.ForeignKey('schueler.id'), nullable=False)
+    datum = db.Column(db.Date, nullable=False)
+    uhrzeit = db.Column(db.Time, nullable=True)
+    dauer = db.Column(db.Integer, nullable=True)
+    besonderheiten = db.Column(db.String(255), nullable=True)
+    sonderfahrt_typ = db.Column(db.String(100), nullable=True)
+
+    schueler = db.relationship('Schueler', backref='protokolle')
