1487928f1f2f20691d5f994ad799499dedfd701a
eyfahrlehrer <info@machmichmobiler.com>
2025-06-10 23:12:03 +0200

    Update models.py

--- models.py
+++ models.py
@@ -17,23 +17,3 @@ class Schueler(db.Model):
     theorie_bestanden = db.Column(db.Boolean, default=False)
     fahrerlaubnisklasse = db.Column(db.String(10))
     erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-    schueler = db.relationship('Schueler', backref='protokolle')
-
-
-class Schueler(db.Model):
-    __tablename__ = 'schueler'
-    id = db.Column(db.Integer, primary_key=True)
-    vorname = db.Column(db.String(50), nullable=False)
-    nachname = db.Column(db.String(50), nullable=False)
-    geburtsdatum = db.Column(db.Date)
-    adresse = db.Column(db.Text)
-    plz = db.Column(db.String(10))
-    ort = db.Column(db.String(100))
-    mobilnummer = db.Column(db.String(20))
-    sehhilfe = db.Column(db.Boolean, default=False)
-    theorie_bestanden = db.Column(db.Boolean, default=False)
-    fahrerlaubnisklasse = db.Column(db.String(10))  # 👈 NEU
-    erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
-
