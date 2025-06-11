277a9fc8c653b6b7f57dee42f89b44f96019fa66
eyfahrlehrer <info@machmichmobiler.com>
2025-06-11 22:23:32 +0200

    Update models.py

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+# Schülerdaten
 class Schueler(db.Model):
     __tablename__ = 'schueler'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,17 +17,32 @@ class Schueler(db.Model):
     sehhilfe = db.Column(db.Boolean, default=False)
     theorie_bestanden = db.Column(db.Boolean, default=False)
     fahrerlaubnisklasse = db.Column(db.String(10))
+    geschlecht = db.Column(db.String(20))
     erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
 
+# Fahrzeugdaten
+class Fahrzeug(db.Model):
+    __tablename__ = 'fahrzeuge'
+    id = db.Column(db.Integer, primary_key=True)
+    bezeichnung = db.Column(db.String(100), nullable=False)
+    typ = db.Column(db.String(50))  # Schalter oder Automatik
+    kennzeichen = db.Column(db.String(20))
+
+# Fahrstunden-Protokoll
 class Fahrstundenprotokoll(db.Model):
     __tablename__ = 'fahrstundenprotokoll'
 
     id = db.Column(db.Integer, primary_key=True)
     schueler_id = db.Column(db.Integer, db.ForeignKey('schueler.id'), nullable=False)
     datum = db.Column(db.Date, nullable=False)
-    uhrzeit = db.Column(db.Time, nullable=True)
-    dauer = db.Column(db.Integer, nullable=True)
-    besonderheiten = db.Column(db.String(255), nullable=True)
-    sonderfahrt_typ = db.Column(db.String(100), nullable=True)
+    uhrzeit = db.Column(db.Time)
+    dauer = db.Column(db.Integer)
+    besonderheiten = db.Column(db.String(255))
+    sonderfahrt_typ = db.Column(db.String(100))
+    fahrzeug_id = db.Column(db.Integer, db.ForeignKey('fahrzeuge.id'))
+    zahlungsstatus = db.Column(db.String(20))  # z. B. bar, EC, offen
+    preis = db.Column(db.Numeric(6, 2))
+    erstellt_am = db.Column(db.DateTime, default=datetime.utcnow)
 
     schueler = db.relationship('Schueler', backref='protokolle')
+    fahrzeug = db.relationship('Fahrzeug', backref='einsatzzeiten')
