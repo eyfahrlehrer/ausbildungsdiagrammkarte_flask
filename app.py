f6a79a1b4470cde54babe90733fd654fc79a977f
eyfahrlehrer <info@machmichmobiler.com>
2025-06-12 02:08:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask
 from models import db
 from main.routes import main  # Blueprint
 
-
-
 app = Flask(__name__)
 app.config.from_object("config.Config")
 
 db.init_app(app)
+
+# Tabelle anlegen (nur nötig, wenn keine Migration via Alembic verwendet wird)
+with app.app_context():
+    db.create_all()
+
 app.register_blueprint(main)
 
 if __name__ == "__main__":
